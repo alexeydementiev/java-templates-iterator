e456b4385312277118d3dedcc046931fa492a5e2
alexeydementiev <adementiev@flyxo.com>
2022-05-20 16:19:44 +0300

    first commit

---
+++ src/main/java/classes/Randoms.java
@@ -0,0 +1,35 @@
+package classes;
+
+import java.util.Iterator;
+import java.util.Random;
+
+public class Randoms implements Iterable<Integer> {
+
+	protected Random random;
+	protected int min, max;
+
+	public Randoms(int min, int max) {
+		this.random = new Random();
+		this.max = max;
+		this.min = min;
+	}
+
+	@Override
+	public Iterator<Integer> iterator() {
+		return new Iterator<>() {
+			@Override
+			public boolean hasNext() {
+				return true;
+			}
+
+			@Override
+			public Integer next() {
+				return random
+						.ints(1, min, max + 1)
+						.findFirst()
+						.getAsInt();
+			}
+		};
+	}
+
+}
